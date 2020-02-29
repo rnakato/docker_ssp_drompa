6682c70ebe6424f60e8e829af65ae053a7246429
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2020-02-29 12:14:02 +0900

    add Dockerfile.dorowu

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
-for tag in latest
+for tag in latest 18.04
 do
     docker build -t rnakato/ssp_drompa:$tag .
     docker push rnakato/ssp_drompa:$tag
 done
+
+tag=dorowu-bionic
+docker build -f Dockerfile.dorowu -t rnakato/ssp_drompa:$tag .
+docker push rnakato/ssp_drompa:$tag
